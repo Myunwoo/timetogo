b7695424ee5942ebebc4143206c9b804069fc42d
Munwoo <fddsgt123@gmail.com>
2024-06-09 19:02:18 +0900

    firebase

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, browserSessionPersistence } from 'firebase/auth';
 
 import {
   FIREBASE_API_KEY,
@@ -21,5 +21,11 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+const auth = initializeAuth(app, {
+  persistence: browserSessionPersistence,
+  popupRedirectResolver: undefined,
+});
+
+
+export { auth };
 export default app;
